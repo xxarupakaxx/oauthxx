9514a44b31f546ebc6fc1805fc1806bc4a30f491
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-09-21 16:26:10 +0900

    aaa

--- client/app/app.go
+++ client/app/app.go
@@ -1,1 +1,70 @@
-package app
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/caos/oidc/pkg/client/rp"
+	"github.com/caos/oidc/pkg/oidc"
+	"github.com/caos/oidc/pkg/utils"
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	callbackPath = "/auth/callback"
+	key = []byte("test1234test1234")
+)
+
+func main() {
+	clientID := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	keyPath := os.Getenv("KEY_PATH")
+	issuer := os.Getenv("ISSUER")
+	port := os.Getenv("PORT")
+	scopes := strings.Split(os.Getenv("SCOPES"), " ")
+
+	redirectURI := fmt.Sprintf("http://localhost:%v%v", port, callbackPath)
+	cookieHandler :=utils.NewCookieHandler(key,key,utils.WithUnsecure())
+
+	options := []rp.Option{
+		rp.WithCookieHandler(cookieHandler),
+		rp.WithVerifierOpts(rp.WithIssuedAtOffset(5*time.Second)),
+	}
+	if clientSecret=="" {
+		options = append(options,rp.WithPKCE(cookieHandler))
+	}
+	if keyPath != "" {
+		options = append(options,rp.WithClientKey(keyPath))
+	}
+
+	provider ,err := rp.NewRelyingPartyOIDC(issuer,clientID,clientSecret,redirectURI,scopes,options...)
+	if err != nil {
+		logrus.Fatalf("error creating provider %s",err.Error())
+	}
+
+	state := func() string {
+		return uuid.New().String()
+	}
+
+	http.Handle("/login",rp.AuthURLHandler(state,provider))
+
+	marshalUserinfo := func(w http.ResponseWriter,r *http.Request,tokens *oidc.Tokens, state string , rp rp.RelyingParty,info oidc.UserInfo) {
+		data, err := json.Marshal(info)
+		if err != nil {
+			http.Error(w,err.Error(),http.StatusInternalServerError)
+			return
+		}
+		w.Write(data)
+	}
+
+	http.Handle(callbackPath,rp.CodeExchangeHandler(rp.UserinfoCallback(marshalUserinfo),provider))
+
+	lis := fmt.Sprintf("127.0.0.1:%s", port)
+	logrus.Infof("listening on http://%s/", lis)
+	logrus.Fatal(http.ListenAndServe("127.0.0.1:"+port, nil))
+
+}
--- go.mod
+++ go.mod
@@ -4,14 +4,23 @@ go 1.17
 
 require (
 	github.com/caos/oidc v0.15.10 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/labstack/echo/v4 v4.6.0 // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.0.0-20210913180222-943fd674d43e // indirect
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
 	golang.org/x/sys v0.0.0-20210910150752-751e447fb3d0 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 )

527624a4853e2615e8bbe815d7b2ef6d67a4a341
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-09-23 02:21:37 +0900

    :hammer: gihub.goの写経

--- client/github/github.go
+++ client/github/github.go
@@ -1,1 +1,56 @@
-package github
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/caos/oidc/pkg/client/rp"
+	"github.com/caos/oidc/pkg/client/rp/cli"
+	"github.com/caos/oidc/pkg/utils"
+	"github.com/google/go-github/v31/github"
+	"github.com/google/uuid"
+	"golang.org/x/oauth2"
+	githubOAuth "golang.org/x/oauth2/github"
+	"os"
+)
+
+var (
+	callbackPath string = "/orbctl/github/callback"
+	key []byte =[]byte("test1234test1234")
+)
+
+func main() {
+	clientId :=os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	port :=os.Getenv("PORT")
+
+	rpConfig := &oauth2.Config{
+		ClientID:     clientId,
+		ClientSecret: clientSecret,
+		Endpoint:     githubOAuth.Endpoint,
+		RedirectURL:  fmt.Sprintf("http://localhost:%v%v", port, callbackPath),
+		Scopes:       []string{"repo","repo_deployment"},
+	}
+
+	ctx := context.Background()
+	cookieHandler := utils.NewCookieHandler(key,key,utils.WithUnsecure())
+	relyingParty,err := rp.NewRelyingPartyOAuth(rpConfig,rp.WithCookieHandler(cookieHandler))
+	if err != nil {
+		fmt.Printf("error creating relyaing party: %v",err)
+		return
+	}
+	state := func() string {
+		return uuid.New().String()
+	}
+
+	token := cli.CodeFlow(ctx,relyingParty,callbackPath,port,state)
+
+	client := github.NewClient(relyingParty.OAuthConfig().Client(ctx,token.Token))
+
+	_,_,err = client.Users.Get(ctx,"")
+	if err != nil {
+		fmt.Printf("error %v",err)
+		return
+	}
+	fmt.Printf("call succeeded")
+}
+

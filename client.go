d0b0ddcf58b18218e0ddd489b9758a1d50a72569
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-10-11 13:17:35 +0900

    handlerの追加

--- client.go
+++ client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"io"
+	"net/http"
 	"text/template"
 )
 
@@ -18,10 +19,34 @@ func main() {
 	e :=echo.New()
 
 	e.Renderer = t
+	e.Static("/static","template/client")
+
+	e.GET("/",indexClient)
+	e.GET("/authorize",authorizeClient)
+	e.GET("/callback",callbackClient)
+	e.GET("/fetch_resource",resourceClient)
 
 
 	e.Logger.Fatal(e.Start(":9000"))
 }
+
+func resourceClient(c echo.Context) error {
+	return c.Render(http.StatusOK,"data",nil)
+
+}
+
+func callbackClient(c echo.Context) error {
+	return c.Render(http.StatusOK,"index",nil)
+}
+
+func authorizeClient(c echo.Context) error {
+	return c.Redirect(http.StatusMovedPermanently,"https://twitter.com/home")
+}
+
+func indexClient(c echo.Context) error {
+	return c.Render(http.StatusOK,"index",nil)
+}
+
 
 
 
